bb8d5c6479faefc3ab61de95fc011b24860305ba
Aaditya Gupta <aadityagupta400@gmail.com>
2024-02-29 03:13:45 +0530

    fix: main content header fixed position and y-overflow

--- src/app/_components/mainContent/header.tsx
+++ src/app/_components/mainContent/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <header className="text-white p-5">
+    <header className="text-white p-5 fixed top-0 w-full z-10">
       {/* Content of the header heuheu*/}
       huehue
     </header>
--- src/app/_components/mainContent/mainContent.tsx
+++ src/app/_components/mainContent/mainContent.tsx
@@ -5,15 +5,17 @@ import CardCarousel from "./carousel";
 import { trendingCards } from "@/app/data/trendingSongs";
 import { newReleaseCards } from "@/app/data/newSongs";
 
-
 export default function MainContent() {
   return (
-    <div className="bg-bgs w-full h-full">
+    <div className="bg-bgs w-full h-full flex flex-col overflow-y-auto">
       <div>
         <Header />
-        <Hero />
-        <CardCarousel title="Trending" cards={trendingCards} />
-        <CardCarousel title="New Releases" cards={newReleaseCards} />
+        <div className="pt-24">
+          <Hero />
+          <CardCarousel title="Trending" cards={trendingCards} />
+          <CardCarousel title="New Releases" cards={newReleaseCards} />
+          <div className="h-[200vh] bg-blue-200"></div>
+        </div>
       </div>
     </div>
   );
