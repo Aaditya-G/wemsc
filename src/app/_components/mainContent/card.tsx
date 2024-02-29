9c5fd1ca8401dbe3505e92c27626e7c2db137af0
Aaditya Gupta <aadityagupta400@gmail.com>
2024-02-29 23:02:17 +0530

    chore: card hover effects

--- src/app/_components/mainContent/card.tsx
+++ src/app/_components/mainContent/card.tsx
@@ -4,15 +4,20 @@ import Image from "next/image";
 
 const Card: React.FC<{ card: ICard }> = ({ card }) => {
   return (
-    <div className="flex-shrink-0 w-[15vw] flex flex-col gap-2">
+    <div className="flex-shrink-0 w-[15vw] flex flex-col gap-2 cursor-pointer hover:text-blue-600 transition-all duration-300">
       {/* <img className="w-full" src={card.imageUrl} alt={card.title} /> */}
-      <Image
-        src={pic3}
-        alt="pic3"
-        width={300}
-        height={300}
-        className="aspect-square rounded-2xl"
-      />
+      <div className="relative overflow-hidden aspect-square rounded-2xl">
+        <div className="transition-transform duration-300 hover:scale-105">
+          <Image
+            src={pic3}
+            alt="pic3"
+            width={300}
+            height={300}
+            className="aspect-square rounded-2xl hover:opacity-70 transition-all duration-300"
+          />
+        </div>
+      </div>
+
       <div className="font-bold text-xl -mb-2">{card.title}</div>
       <p className="text-gray-700 text-base">{card.artist}</p>
     </div>
